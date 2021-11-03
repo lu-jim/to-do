670ccc15a7ccad47cc2bb85e2a03c0c5779f850e
Luis Fernando Jimenez <lu.jim@protonmail.com>
2021-11-03 11:59:58 -0400

    Finish list structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './styles.css';
 
-console.log('Hola panas');
+class toDo {
+  constructor(description) {
+    this.description = description;
+    this.completed = false;
+    this.index = 0;
+  }
+}
+
+const newList = [
+  new toDo('wash the dishes'),
+  new toDo('complete the To Do list project'),
+  new toDo('complete Microverse Program'),
+];
+const todoUl = document.querySelector('#to-do');
+
+newList.forEach((item) => {
+  item.index = newList.indexOf(item);
+  const todoEntry = document.createElement('li')
+  todoEntry.innerHTML = `<input type="checkbox"> ${item.description}`
+  todoUl.appendChild(todoEntry);
+});
